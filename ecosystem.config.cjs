7ce41ccd15304fb3073862aee40e7b17c4ce6c87
Kevin Medeiros <108627831+medeirosnvk@users.noreply.github.com>
2025-10-16 11:46:14 -0300

    wip

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,6 +5,10 @@ module.exports = {
       script: "node",
       args: "./build/server/index.js", // Arquivo gerado após o build
       cwd: "/home/deploy/api-whatsapp-frontend", // opcional, mas recomendado
+      env: {
+        NODE_ENV: "production",
+        PORT: 5175,
+      },
     },
   ],
 };
